6c5a5246f3df8d7fed206f86a5c77ccdef979cb8
TongmengHAI <tongmeng016@gmail.com>
2023-12-29 08:57:52 +0700

    update api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::controller(CategoriesController::class)->group(function(){
+//     Route::get('/categories');
+//     Route::post('/categories');
+//     Route::get('/categories/{categoryId}');
+//     Route::patch('/categories/{categoryId}');
+//     Route::delete('/categories/{categoryId}');
+// });
+
+    Route::get('/categories',function(){
+        return "Get all categories";
+    });
+
+    Route::post('/categories',function(Request $request){
+        return "Create all categories";
+    });
+
+    Route::patch('/categories/{categoryId}',function(Request $request){
+        return "Update 1 category";
+    });
+
+    Route::delete('/categories/{categoryId}',function(Request $request){
+        return "Delete 1 category";
+
+    });
+
+    Route::get('/product',function(){
+        return "Get all products";
+    });
+
+    Route::post('/product',function(Request $request){
+        return "Create all products";
+    });
+
+    Route::patch('/product/{productId}',function(Request $request){
+        return "Update 1 product";
+    });
+
+    Route::delete('/product/{productId}',function(Request $request){
+        return "Delete 1 product";
+
+    });
+
+
+
